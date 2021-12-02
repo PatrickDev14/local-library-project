185ed588d454eacb98c4d2a2072a65bd967bd57d
PatrickDev14 <93887251+PatrickDev14@users.noreply.github.com>
2021-12-02 10:49:33 -0600

    accounts.js update comments

--- src/accounts.js
+++ src/accounts.js
@@ -5,17 +5,17 @@ function findAccountById(accounts, id) {
   return accounts.find((account) => account.id === id);  
 }
 
+//use sort() to return accounts array sorted alphabetically
+//between 2 sort arguments, later alphabetical argument gets +1 to move account index later
 function sortAccountsByLastName(accounts) {
-  //use sort method
   return accounts.sort((accountA, accountB) =>
-    //later alphabetical argument gets +1 to move account index later
     accountA.name.last > accountB.name.last ? 1 : -1
   );
 }
 
 //declare a variable for storing acct ID, and accumulating total borrows
 //use forEach() over books array, and then also borrows array
-//when ID match is satisfied, then accumulate borrows
+//when ID match is satisfied, then accumulate 1 borrows
 function getTotalNumberOfBorrows(account, books) {
   const acctId = account.id;
   let totalBorrows = 0;
