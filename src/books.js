b6baa3e04bd28027404d3e0af37f4160b2ca7151
PatrickDev14 <93887251+PatrickDev14@users.noreply.github.com>
2021-12-02 10:47:16 -0600

    books.js add first 2 functions

--- src/books.js
+++ src/books.js
@@ -1,6 +1,14 @@
-function findAuthorById(authors, id) {}
+/* Requirements - */
 
-function findBookById(books, id) {}
+//return 1 author object from authors array that matches id; use find()
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
+
+//return 1 book object from book array that matches id; use find()
+function findBookById(books, id) {
+  return books.find((book) => book.id === id);
+}
 
 function partitionBooksByBorrowedStatus(books) {}
 
