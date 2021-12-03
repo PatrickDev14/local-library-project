2fd12ae30f21c5b22b985e1bafeba32e9c2caec5
PatrickDev14 <93887251+PatrickDev14@users.noreply.github.com>
2021-12-02 21:09:35 -0600

    update functions and comments
    Add helper function 'booksNotReturned'; update function 'partitionBooksByBorrowedStatus' to use 'booksNotReturned'; update module.exports

--- src/books.js
+++ src/books.js
@@ -1,5 +1,6 @@
-/* Requirements - uses methods: filter(), some(), every()  
-    uses spread operator */
+/* Requirements - uses methods: filter(), some(), every();  
+    uses spread operator;
+    declares and uses helper function 'booksNotReturned' */
 
 //return 1 author object from authors array that matches id; use find()
 function findAuthorById(authors, id) {
@@ -11,23 +12,32 @@ function findBookById(books, id) {
   return books.find((book) => book.id === id);
 }
 
+/*This helper function returns an array of unreturned books, by looking over each books.borrows
+array with filter() and some() methods. */
+function booksNotReturned(books) {
+  //filter() books into a new array that are Not Returned
+  return books.filter((book) =>
+  //some() method looks over book.borrows array, and one returned === false means book is Not Returned
+    book.borrows.some((borrow) => borrow.returned === false)
+    );
+}
 /*filter the books array into 2 arrays. for 1)books.borrows[i].returned: has a false;
+can use the helper function 'booksNotReturned' from line 17;
  and for 2) books.borrows[i].returned: are all true - these are returnedBooks;
  then return 2 arrays inside 1, can use the spread operator when combining them;
 declare variables for new arrays
+**great struggle initially, since while looking at function-instructions.md, I was only seeing
+books.borrows objects only having 1 'returned: value' -- look at all the data !!
 */
 function partitionBooksByBorrowedStatus(books) {
-  //filter() books into books that are NotReturned
-  let booksNotReturned = books.filter((book) =>
-  //some() helper function looks over book.borrows array, and one returned === false means book is NotReturned
-    book.borrows.some((borrow) => borrow.returned === false)
-    );
+  //declare notReturned; use helper function booksNotReturned to build array of unreturned books
+  var notReturned = booksNotReturned(books);
   //filter() books into books that are returned
   let returnedBooks = books.filter((book) =>
   //every() looks over book.borrows array for every return === true
     book.borrows.every((borrow) => borrow.returned === true)
     );
-  return [[...booksNotReturned], [...returnedBooks]];  
+  return [[...notReturned], [...returnedBooks]];  
 }
 
 function getBorrowersForBook(book, accounts) {}
@@ -37,4 +47,5 @@ module.exports = {
   findBookById,
   partitionBooksByBorrowedStatus,
   getBorrowersForBook,
+  booksNotReturned,
 };
