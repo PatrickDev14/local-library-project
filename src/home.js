abb81f4345d983fcd2598d63618e8dd692f1c521
PatrickDev14 <93887251+PatrickDev14@users.noreply.github.com>
2021-12-03 21:31:14 -0600

    update functions
    updated the last function; added helper function chooseTopFive()

--- src/home.js
+++ src/home.js
@@ -1,8 +1,16 @@
-/* Requirements - uses helper function 'booksNotReturned'*/
+/* Requirements - this file uses helper function 'booksNotReturned';
+    uses methods: map(), reduce() */
 
 //require the helper function booksNotReturned from books.js file
 const {booksNotReturned} = require("./books");
 
+/*chooseTopFive is a helper function with an array parameter, that sorts
+the array by the count value, and uses slice() to return an array with the 
+first 5 objects of the sorted array */
+function chooseTopFive(array) {
+  return array.sort((a,b) =>
+    (a.count < b.count ? 1: -1)).slice(0,5);
+}
 
 //return the number of book objects inside the books array; use .length
 //should return zero if the array is empty
@@ -17,12 +25,21 @@ function getTotalAccountsCount(accounts) {
 }
 
 //return number of books currently checked out where a books.borrows.returned === false
-//we can return .length of the array of unreturned books from the helper function 'booksNotReturned'
+//use reduce() and accumulate the results where book.borrows.returned === false
 function getBooksBorrowedCount(books) {
-  return booksNotReturned(books).length;  
+  let borrowed = books.reduce((result, book) => {
+    for (let i=0; i < book.borrows.length; i++) {
+      if (book.borrows[i].returned === false) {
+        result++;
+      }
+    }
+    return result;
+  }, 0)
+  return borrowed;   
 }
 
-/* for a count variable, */
+/* return an array of genre objects that are {name: book.genre, count: filter(books).length} 
+ */
 function getMostCommonGenres(books) {}
 
 /* return an array of book objects that are {name: book title, count: borrow arrays length}
@@ -33,12 +50,29 @@ function getMostPopularBooks(books) {
   let bookBorrows = books.map((book) => {
     return { name: book.title, count: book.borrows.length };
 });
-  return bookBorrows.sort((a,b) =>
-    (a.count < b.count ? 1: -1)).slice(0,5);
+  return chooseTopFive(bookBorrows);
 }
 
-
-function getMostPopularAuthors(books, authors) {}
+/*return a sliced array with the 5 authors who have the most read books in total
+build first array with objects like { name: "author name", count: 0} -- not counting reads yet
+build second array when author.id is in the book object, and add the book.borrows.length to 'count'
+return array with top 5 authors using 'function chooseTopFive' */
+function getMostPopularAuthors(books, authors) {
+  let authorsReads = [];
+  authors.forEach((author) => {
+    let authorX = {
+      name: `${author.name.first} ${author.name.last}`,
+      count: 0
+    };
+    books.forEach((book) => {
+    if (book.authorId === author.id) {
+      authorX.count += book.borrows.length;
+    }
+  });
+  authorsReads.push(authorX);
+  });
+  return chooseTopFive(authorsReads);
+}
 
 module.exports = {
   getTotalBooksCount,
@@ -47,4 +81,5 @@ module.exports = {
   getMostCommonGenres,
   getMostPopularBooks,
   getMostPopularAuthors,
+  chooseTopFive,
 };
